457bd36643e949aa2d05a8028107c8164ba9dc34
Jiya <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2022-01-04 22:32:31 +0530

    Update sheets_setup.py

--- setup/sheets_setup.py
+++ setup/sheets_setup.py
@@ -6,6 +6,6 @@ scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file",
          "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name('static/key/api.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('../static/key/api.json', scope)
 client = gspread.authorize(credentials)
 sheet = client.open_by_key("1jGMF0OBt8x20C8eLX9u6Qx8sADyy870kLyxE0HO_U2g").sheet1
